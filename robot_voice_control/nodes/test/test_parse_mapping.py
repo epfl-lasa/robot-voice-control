3fd65d98e4d92df7e851b240f17b4692e4b5d938
Felix Duvallet <felix.duvallet@epfl.ch>
2015-10-16 13:59:17 +0200

    Fake the parameter server by storing the resulting dictionary.

--- robot_voice_control/nodes/test/test_parse_mapping.py
+++ robot_voice_control/nodes/test/test_parse_mapping.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import unittest
+
+import rospy
 import rostest
 import std_msgs
+import rosparam
 
 from robot_voice_control.nodes.message_control import LanguageToMessageTranslator
 
@@ -16,6 +19,27 @@ class TestCase(unittest.TestCase):
     def setupTranslator(self):
         self.translator = LanguageToMessageTranslator()
 
+    def setupParameters(self):
+        self.control_param_name = '/test_topic'
+
+        # Since we cannot access the parameter server directly, fake it by
+        # just storing the resulting parameter dictionary here. This is the same
+        # as loading the test_params.yaml file and then calling
+        # rospy.get_param(/test_topic)
+        self.params = {'basic_topic':
+                           {'input': 'output',
+                            'input with spaces': 'output with spaces'},
+                       'more': {'complicated': {'topic': {'input': 42}}},
+                       'topics': [{'basic_topic': 'String'},
+                                  {'more/complicated/topic': 'Int32'},
+                                  {'/global/topic': 'Float32'},
+                                  {'unknown_type': 'Unknown'}],
+                       'unknown_topic': {'input': 'does not matter'},
+                       'unknown_type': {'input': 'does not matter'}}
+
+        # This is a global topic, it is stored separately.
+        self.global_param = {'input': 3.14159}
+
     def testSetup(self):
         self.setupTranslator()
         self.assertIsNotNone(self.translator)
@@ -23,7 +47,7 @@ class TestCase(unittest.TestCase):
     def test_parse_command_map_wrong_type(self):
         not_a_type = 'NotAType'
         ret = LanguageToMessageTranslator.parse_command_mapping(
-            'topic_name', not_a_type, {})
+            'topic_name', not_a_type, {'a': 'b'})
         self.assertIsNotNone(ret)
 
     # Test string->msg type conversions.
@@ -42,6 +66,29 @@ class TestCase(unittest.TestCase):
     def test_string_to_type_Other(self):
         ret = LanguageToMessageTranslator.string_to_type['NotAType']
         self.assertIsNone(ret)
+
+    def test_setup_params(self):
+        self.setupParameters()
+
+        self.assertEqual(4, len(self.params['topics']))
+
+        # Ensure both relative topics are *under* the params
+        self.assertIn('basic_topic', self.params)
+        self.assertIn('more', self.params)  # slashes become sub-dicts
+        self.assertIn('unknown_type', self.params)
+        self.assertIn('unknown_topic', self.params)
+
+        # Basic params
+        basic = self.params['basic_topic']
+        self.assertEqual(2, len(basic))
+        self.assertIn('input', basic)
+        self.assertIn('input with spaces', basic)
+
+    def ztest_parse_command_map(self):
+        self.fail('create an input map')
+        self.fail('set the type')
+        self.fail('set the topic name')
+        self.fail('ensure the correct output')
 
 
 
