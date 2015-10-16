d5bf3dbc51521db46440a1f2f9b0a8086fdbb386
Felix Duvallet <felix.duvallet@epfl.ch>
2015-10-16 10:08:30 +0200

    Convert a string representation of a type to the actual message type.

--- robot_voice_control/nodes/message_control.py
+++ robot_voice_control/nodes/message_control.py
@@ -9,6 +9,9 @@
 # should be mapped to which command.
 
 import rospy
+from collections import defaultdict
+
+import std_msgs
 from std_msgs.msg import String
 
 class LanguageToMessageTranslator(object):
@@ -16,6 +19,7 @@ class LanguageToMessageTranslator(object):
     def __init__(self):
 
         nl_command_topic = '/nl_command_parsed'
+
 
         # map of nl_command -> (topic, message)
         self.nl_command_map = {'home': ('/allegroHand/lib_cmd', 'home'),
@@ -25,7 +29,51 @@ class LanguageToMessageTranslator(object):
                                }
         rospy.Subscriber(nl_command_topic, String, self.nl_command_callback)
 
+    def load_nl_command_map(self, control_param_name):
+
+        if not rospy.has_param(control_param_name):
+            rospy.logerr('Cannot proceed without the control parameter: {}'.format(control_param_name))
+            rospy.signal_shutdown('Cannot proceed without parameter.')
+
+        param = rospy.get_param(control_param_name)
+        rospy.loginfo('param: {}'.format(param))
+        assert 'topics' in param
+
+        # Convert a list of dictionaries to a list of tuples.
+        topics_and_types = [x.items()[0] for x in param['topics']]
+
+        for (param_topic_name, topic_type_str) in topics_and_types:
+            if not rospy.has_param(param_topic_name):
+                rospy.logerr('Expected parameter {}'.format(param_topic_name))
+            rospy.loginfo('Getting all command maps that go on topic {}'.format(param_topic_name))
+            command_mapping = rospy.get_param(param_topic_name)
+
+            tmp = LanguageToMessageTranslator.parse_command_mapping(
+                param_topic_name, topic_type_str, command_mapping)
+
+            self.nl_command_map.update(tmp)
+
+
         rospy.loginfo('NL control running, listening to topic: {}.'.format(nl_command_topic))
+
+    @classmethod
+    def parse_command_mapping(cls, topic_name, topic_type_str, command_mapping):
+        ret = {}
+
+        msg_t = cls.string_to_type[topic_type_str]
+        if not msg_t:
+            return ret
+
+        return ret
+
+    # This defaultdict maps a string representation of the message type
+    # (what's stored inside the parameter server) to the actual message type.
+    # By default it returns None (instead of raising an exception) if the
+    # message type is unknown.
+    string_to_type = defaultdict(lambda: None,
+                                 [('String', std_msgs.msg.String),
+                                  ('Int32', std_msgs.msg.Int32),
+                                  ('Float32', std_msgs.msg.Float32)])
 
     def nl_command_callback(self, msg):
         command = msg.data
@@ -48,7 +96,7 @@ def run():
 
     translator = LanguageToMessageTranslator()
 
-    rospy.spin()
+    ####rospy.spin()
 
 
 if __name__ == '__main__':
--- robot_voice_control/nodes/test/test_parse_mapping.py
+++ robot_voice_control/nodes/test/test_parse_mapping.py
@@ -2,23 +2,51 @@
 
 import unittest
 import rostest
+import std_msgs
 
 from robot_voice_control.nodes.message_control import LanguageToMessageTranslator
+
 
 class TestCase(unittest.TestCase):
 
     def setUp(self):
+        #
+        pass
+
+    def setupTranslator(self):
         self.translator = LanguageToMessageTranslator()
 
     def testSetup(self):
+        self.setupTranslator()
         self.assertIsNotNone(self.translator)
 
-    def test_something(self):
-        self.assertTrue(True)
-        pass
+    def test_parse_command_map_wrong_type(self):
+        not_a_type = 'NotAType'
+        ret = LanguageToMessageTranslator.parse_command_mapping(
+            'topic_name', not_a_type, {})
+        self.assertIsNotNone(ret)
+
+    # Test string->msg type conversions.
+    def test_string_to_type_String(self):
+        ret = LanguageToMessageTranslator.string_to_type['String']
+        self.assertEqual(std_msgs.msg.String, ret)
+
+    def test_string_to_type_Float(self):
+        ret = LanguageToMessageTranslator.string_to_type['Float32']
+        self.assertEqual(std_msgs.msg.Float32, ret)
+
+    def test_string_to_type_Int(self):
+        ret = LanguageToMessageTranslator.string_to_type['Int32']
+        self.assertEqual(std_msgs.msg.Int32, ret)
+
+    def test_string_to_type_Other(self):
+        ret = LanguageToMessageTranslator.string_to_type['NotAType']
+        self.assertIsNone(ret)
+
+
 
 
 if __name__ == '__main__':
-    rostest.rosrun('test_parse_mapping', 'test_parse_mapping', TestCase)
+    rostest.rosrun('robot_voice_control', 'test_parse_mapping', TestCase)
 
 __author__ = 'felixd'
